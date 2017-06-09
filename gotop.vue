6ec24b354bf7e9a21d75c2128b6514cb822c678f
Evan <wanlixi@users.noreply.github.com>
2017-06-09 11:50:20 +0800

    Add files via upload

---
+++ gotop.vue
@@ -0,0 +1,37 @@
+<!-- 
+【功能描述】：回到顶部
+ -->
+<template>
+  <div class="gotop-box"><div class="gotop" @click="gototop"></div></div>
+</template>
+<script type="text/babel">
+import $ from 'jquery'
+export default {
+  mounted () {
+    $(window).on('scroll', function(){
+      if ($(this).scrollTop() > 0) {
+        $('.gotop').fadeIn()
+      } else {
+        $('.gotop').fadeOut()
+      }
+    })
+  },
+  methods: {
+    gototop () {
+      $('body,html').animate({ scrollTop: 0 }, 800)
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.gotop-box
+  position: fixed
+  right: 30px
+  bottom: 100px
+  .gotop
+    display: none
+    background: url('~assets/img/common/gototop.png') no-repeat
+    width: 51px
+    height: 40px
+    cursor: pointer
+</style>
